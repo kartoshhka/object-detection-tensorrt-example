c4907a76b66679f5ce1485ba4b868dbf063c8a14
Dmitry Mikushin <dmitry@kernelgen.org>
2019-07-14 17:04:30 +0300

    Cleaning up detectrt.py

--- src/detectrt.py
+++ src/detectrt.py
@@ -17,8 +17,8 @@ import utils.boxes as boxes_utils # Drawing bounding boxes
 import utils.coco as coco_utils # COCO dataset descriptors
 from utils.paths import PATHS # Path management
 
-import pycuda.driver as cuda
-import pycuda.autoinit
+# The video stream to capture for processing
+VIDEO_STREAM = 'http://cam10.zentrale.sip-scootershop.de:10012/axis-cgi/mjpg/video.cgi?resolution=1024x768'
 
 # Default calibration dataset
 VOC_DEVKIT = 'VOCdevkit/VOC2007/JPEGImages'
@@ -76,7 +76,7 @@ def fetch_prediction_field(field_name, detection_out, pred_start_idx):
     """
     return detection_out[pred_start_idx + TRT_PREDICTION_LAYOUT[field_name]]
 
-def analyze_prediction(detection_out, pred_start_idx, img_pil):
+def annotate_prediction(detection_out, pred_start_idx, img_pil):
     image_id = int(fetch_prediction_field("image_id", detection_out, pred_start_idx))
     label = int(fetch_prediction_field("label", detection_out, pred_start_idx))
     confidence = fetch_prediction_field("confidence", detection_out, pred_start_idx)
@@ -101,30 +101,17 @@ def parse_commandline_arguments():
 
     # Define script command line arguments
     parser = argparse.ArgumentParser(description='Run object detection inference on input image.')
-    parser.add_argument('--input_img_path', metavar='INPUT_IMG_PATH',
-        help='an image file to run inference on')
     parser.add_argument('-p', '--precision', type=int, choices=[32, 16, 8], default=32,
         help='desired TensorRT float precision to build an engine with')
     parser.add_argument('-b', '--max_batch_size', type=int, default=1,
         help='max TensorRT engine batch size')
-    parser.add_argument('-w', '--workspace_dir',
-        help='sample workspace directory')
-    parser.add_argument('-fc', '--flatten_concat',
-        help='path of built FlattenConcat plugin')
     parser.add_argument('-d', '--calib_dataset', default=VOC_DEVKIT,
         help='path to the calibration dataset')
-    parser.add_argument('-c', '--camera', default=True,
-        help='if True, will run webcam application')
+    parser.add_argument('-c', '--camera', default=VIDEO_STREAM,
+        help='video device path or stream URL')
 
     # Parse arguments passed
     args = parser.parse_args()
-
-    # Set FlattenConcat TRT plugin path and
-    # workspace dir path if passed by user
-    if args.flatten_concat:
-        PATHS.set_flatten_concat_plugin_path(args.flatten_concat)
-    if args.workspace_dir:
-        PATHS.set_workspace_dir_path(args.workspace_dir)
 
     try:
         os.makedirs(PATHS.get_workspace_dir_path())
@@ -133,15 +120,6 @@ def parse_commandline_arguments():
 
     # Verify Paths after adjustments. This also exits script if verification fails
     PATHS.verify_all_paths()
-
-    # Fetch TensorRT engine path and datatype
-    args.trt_engine_datatype = TRT_PRECISION_TO_DATATYPE[args.precision]
-    args.trt_engine_path = PATHS.get_engine_path(args.trt_engine_datatype,
-        args.max_batch_size)
-    try:
-        os.makedirs(os.path.dirname(args.trt_engine_path))
-    except:
-        pass
 
     return args
 
@@ -160,9 +138,9 @@ def main():
             tar.extractall()
 
     if not os.path.exists(args.calib_dataset):
-        raise IOError('Calibrarion dataset does not exist: %s' % args.calib_dataset)
+        raise IOError('Calibration dataset does not exist: %s' % args.calib_dataset)
 
-    # Load our plugins
+    # Load necessary TensorRT plugins
     library_path = os.path.join(os.path.dirname(__file__), 'libnvinfer_plugin.so')
     plugins = ctypes.CDLL(library_path)
     plugins.initLibNvInferPlugins.restype = None
@@ -175,45 +153,50 @@ def main():
     if not os.path.exists(ssd_model_uff_path):
         model_utils.prepare_ssd_model(MODEL_NAME)
 
-    print("TRT ENGINE PATH %s" % args.trt_engine_path)
+    # Fetch TensorRT engine path and datatype
+    trt_engine_datatype = TRT_PRECISION_TO_DATATYPE[args.precision]
+    trt_engine_path = PATHS.get_engine_path(trt_engine_datatype, args.max_batch_size)
+    try:
+        os.makedirs(os.path.dirname(trt_engine_path))
+    except:
+        pass
 
     # Set up all TensorRT data structures needed for inference
     trt_inference_wrapper = inference_utils.TRTInference(
-        args.trt_engine_path, ssd_model_uff_path,
-        trt_engine_datatype=args.trt_engine_datatype,
+        trt_engine_path, ssd_model_uff_path,
+        trt_engine_datatype=trt_engine_datatype,
         calib_dataset = args.calib_dataset,
         batch_size=args.max_batch_size)
 
-    '''if args.camera == True:
-        print('Running webcam:', args.camera)'''
     # Define the video stream
-    cap = cv2.VideoCapture('http://cam10.zentrale.sip-scootershop.de:10012/axis-cgi/mjpg/video.cgi?resolution=1024x768')
+    stream = cv2.VideoCapture(args.camera)
 
-        # Loop for running inference on frames from the webcam
+    # Loop for running inference on frames from the webcam
     while True:
-      # Read frame from camera (and expand its dimensions to fit)
-      ret, image_np = cap.read()
-      if not ret:
-        print('Error opening videostream')
-        break
-      else:
-        # Actually run inference
-        detection_out, keep_count_out = trt_inference_wrapper.infer_webcam(image_np)
+        # Read frame from camera (and expand its dimensions to fit)
+        ret, image_np = stream.read()
+        if not ret:
+            print('Error reading video stream from %s' % args.camera)
+            break
+        else:
+            # Run inference
+            detection_out, keep_count_out = trt_inference_wrapper.infer_webcam(image_np)
 
-        # Overlay the bounding boxes on the image
-        # let analyze_prediction() draw them based on model output
-        img_pil = Image.fromarray(image_np)
-        prediction_fields = len(TRT_PREDICTION_LAYOUT)
-        for det in range(int(keep_count_out[0])):
-          analyze_prediction(detection_out, det * prediction_fields, img_pil)
-        final_img = np.asarray(img_pil)
+            # Overlay the bounding boxes on the image
+            # let annotate_prediction() draw them based on model output
+            img_pil = Image.fromarray(image_np)
+            prediction_fields = len(TRT_PREDICTION_LAYOUT)
+            for det in range(int(keep_count_out[0])):
+                annotate_prediction(detection_out, det * prediction_fields, img_pil)
+            final_img = np.asarray(img_pil)
 
-        # Display output
-        cv2.imshow('object detection', final_img)
+            # Display output
+            cv2.imshow('object detection', final_img)
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-          cv2.destroyAllWindows()
-          break
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
 
 if __name__ == '__main__':
     main()
+
